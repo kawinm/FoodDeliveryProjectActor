84b2b9183bd6669b40597c8ea04ea6f9c684f5bf
Kawin M <kawinstark@gmail.com>
2022-03-30 21:12:23 +0530

    Wrap up

--- Tests/run_all_tests.py
+++ Tests/run_all_tests.py
@@ -2,18 +2,15 @@ import os
 
 x = input("Press Enter after ensuring all the services are up and running")
 print("\n\nWhich set of test cases do you want to use")
-print("1. Public Test Cases Both sequential and concurrent (Enter input 1)")
-print("2. Sequential Test Cases (Enter input 2)")
-print("3. Concurrent Test Cases (Enter input 3)")
+print("1. Sequential Test Cases (Enter input 1)")
+print("2. Concurrent Test Cases (Enter input 2)")
 print("Any other key to exit")
 
 choice = input()
 if(choice.isnumeric):
     choice = int(choice)
-    if choice == 1 or choice==2 or choice == 3:
+    if choice == 1 or choice==2:
         if choice == 1:
-            os.chdir("public-test-cases")
-        elif choice == 2:
             os.chdir("self-test-cases")
         else:
             os.chdir("concurrent-test-cases")
